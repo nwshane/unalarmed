349d58b0bdd5fb58108e65d34e39ed1a531a15e6
Nathan Shane <nathan.shane@flocabulary.com>
2019-09-29 16:28:59 -0400

    First steps

--- App.tsx
+++ App.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AddAlarmButton from "./components/AddAlarmButton";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "space-between"
   },
+  header: {
+    fontSize: 30
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.header}>Unalarmed</Text>
+        <Text>6 AM</Text>
+        <Text>7 AM</Text>
+      </View>
+      <AddAlarmButton />
+    </View>
+  );
+}
